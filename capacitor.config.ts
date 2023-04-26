8eec0ff705e2a0bf75fe4b499b2839d5a18e1ac0
Tyler <tyler.hicks.21@gmail.com>
2023-04-26 14:09:22 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'snap-a-day',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
