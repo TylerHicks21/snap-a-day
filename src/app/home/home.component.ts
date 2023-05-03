096482d5f49e68eac58ead2c70e3b7f07f7adc20
Tyler <tyler.hicks.21@gmail.com>
2023-05-03 12:17:25 -0400

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,6 @@ import { PhotoService } from './data-access/photo.service';
 import { PhotoListCompontentModule } from './ui/photo-list.component';
 import { SlideShowComponentModule } from '../slideshow/slideshow.component';
 
-//
-//
-//
-//
-
 @Component({
   selector: 'app-home',
   template: `
